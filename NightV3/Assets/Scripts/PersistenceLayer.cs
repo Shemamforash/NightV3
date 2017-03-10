987901871e2adac798c00f837773fea5376c07a8
Shemamforash <samwest2@hotmail.co.uk>
2017-03-10 15:15:34 +0000

    <Fix> Import Data Works

--- NightV3/Assets/Scripts/PersistenceLayer.cs
+++ NightV3/Assets/Scripts/PersistenceLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 public class PersistenceLayer : MonoBehaviour
 {
@@ -30,8 +31,15 @@ public class PersistenceLayer : MonoBehaviour
     public static void LoadGameData()
     {
         TextAsset game_data_file = Resources.Load<TextAsset>("game_data");
-        string[] data_lines = Regex.Split(game_data_file.text, "\n|\r|\r\n");
-        for (int i = 0; i < data_lines.Length; ++i)
+        List<string> data_lines = new List<string>(Regex.Split(game_data_file.text, "\n|\r|\r\n"));
+        for (int i = data_lines.Count - 1; i >= 0; --i)
+        {
+            if (data_lines[i].Trim() == "")
+            {
+                data_lines.RemoveAt(i);
+            }
+        }
+        for (int i = 0; i < data_lines.Count; ++i)
         {
             string next_line = data_lines[i];
             if (next_line.Trim() != "")
@@ -40,10 +48,9 @@ public class PersistenceLayer : MonoBehaviour
                 {
                     string weather_type = next_line.Split(' ')[1];
                     ++i;
-					next_line = data_lines[i];
-                    while (next_line.Trim() != "")
+                    next_line = data_lines[i];
+                    while (next_line.Trim() != "END")
                     {
-                        next_line = data_lines[i];
                         string[] weather_data = next_line.Split(':');
                         if (weather_data.Length == 6)
                         {
@@ -60,16 +67,16 @@ public class PersistenceLayer : MonoBehaviour
                             Debug.Log("incomplete weather data " + next_line);
                         }
                         ++i;
+                        next_line = data_lines[i];
                     }
                 }
                 else if (next_line.Contains("ENVIRONMENT"))
                 {
                     string environment_class = next_line.Split(' ')[1];
                     ++i;
-					next_line = data_lines[i];
-                    while (next_line.Trim() != "")
+                    next_line = data_lines[i];
+                    while (next_line.Trim() != "END")
                     {
-                        next_line = data_lines[i];
                         string[] environment_data = next_line.Split(':');
                         if (environment_data.Length == 9)
                         {
@@ -91,6 +98,8 @@ public class PersistenceLayer : MonoBehaviour
                             Debug.Log("incomplete environment data " + next_line);
                         }
                         ++i;
+                        next_line = data_lines[i];
+
                     }
                 }
                 else
@@ -98,7 +107,7 @@ public class PersistenceLayer : MonoBehaviour
                     string[] line = next_line.Split(':');
                     string key = line[0];
                     float val = float.Parse(line[1].Trim());
-                    game_data.Add(key, val);
+                    game_data[key] = val;
                 }
             }
         }
